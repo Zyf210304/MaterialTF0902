f6ecb6f9bad2904bb724e12e2e523a8c2620b8b9
张亚飞 <672233580@qq.com>
2021-09-02 10:11:56 +0800

    Initial Commit

---
+++ MaterialTF0902/MaterialTF0902App.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialTF0902App.swift
+//  MaterialTF0902
+//
+//  Created by 张亚飞 on 2021/9/2.
+//
+
+import SwiftUI
+
+@main
+struct MaterialTF0902App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
